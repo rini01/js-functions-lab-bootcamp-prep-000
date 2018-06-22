7ed6a46d0af4a52ddd231ce6c8ef245ea7e3940a
fatimah <rini.hasyim@gmail.com>
2018-06-22 00:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
 }
 
+<<<<<<< HEAD
 function holidayCountdown(holiday,days){
+=======
+function holidayCountdown(days,holiday){
+>>>>>>> 7f9f6cf13048263d5f3328ef68c3543dad364099
   return `It's ${days} days until ${holiday}!`
 }
